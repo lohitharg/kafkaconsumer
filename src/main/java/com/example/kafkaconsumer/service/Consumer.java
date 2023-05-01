dc4ee170cc1d1e81477a20f675f712440606eeee
lohit <lohitharg16@gmail.com>
2023-05-01 13:25:30 -0400

    consumer

--- src/main/java/com/example/kafkaconsumer/service/Consumer.java
+++ src/main/java/com/example/kafkaconsumer/service/Consumer.java
@@ -2,6 +2,8 @@ package com.example.kafkaconsumer.service;
 
 
 import com.example.kafkaconsumer.model.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -12,7 +14,10 @@ import org.springframework.stereotype.Service;
 
 public class Consumer {
     @KafkaListener(topics="firsttopic",groupId = "0")
-    public void consumefirstmessage(Employee em){
+    public void consumefirstmessage(String st) throws JsonProcessingException {
+
+        ObjectMapper ob=new ObjectMapper();
+        Employee em=ob.readValue(st,Employee.class);
 System.out.println("message"+em);
     }
 
